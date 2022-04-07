a79dba1a06bc17b0478178d14489c50ad133e8ce
Audun Bjørnerud Mo <audun.mo@remarkable.no>
2022-04-07 15:17:11 +0200

    fix: add basic script fetcher

---
+++ pkg/script_fetcher/script_fetcher.go
@@ -0,0 +1,63 @@
+package scriptfetcher
+
+import (
+	"io"
+	"net/http"
+	"net/url"
+)
+
+type FetcherConfig struct {
+  ignoreHosts []string
+}
+
+type Fetcher struct {
+  config FetcherConfig
+}
+
+// The result of a Fetch call if it skipped a URL
+const SKIPPED = "SKIPPED"
+
+func New(config FetcherConfig) *Fetcher {
+  return &Fetcher{
+    config: config,
+  }
+}
+
+func (f *Fetcher) ShouldSkip(scriptUrl string) (bool, error) {
+  u, err := url.Parse(scriptUrl)
+  if err != nil {
+    return false, err
+  }
+
+  host := u.Hostname()
+  for _, ignoredHost := range f.config.ignoreHosts {
+    if host == ignoredHost {
+      return true, nil
+    }
+  }
+
+  return false, nil
+}
+
+func (f *Fetcher) Fetch(scriptUrl string) (string, error) {
+  skip, err := f.ShouldSkip(scriptUrl)
+  if err != nil {
+    return "", err
+  }
+ 
+  if skip {
+    return SKIPPED, nil
+  }
+
+  r, err := http.Get(scriptUrl)
+  if err != nil {
+    return "", err
+  }
+
+  body, err := io.ReadAll(r.Body)
+  if err != nil {
+    return "", err
+  }
+
+  return string(body), nil
+}
