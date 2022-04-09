d8617de8ffa060a91d78c7ce5143c27808d82320
Audun Bjørnerud Mo <audun.mo@remarkable.no>
2022-04-09 13:09:13 +0200

    fix: dont replace existing integrity checks

--- pkg/injector/injector.go
+++ pkg/injector/injector.go
@@ -6,6 +6,8 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+// Inject finds the given script tag, and then adds the given integrity hash
+// Note, it skips 
 func Inject(markup, scriptUrl, integrity string) (string, error) {
   doc, err := goquery.NewDocumentFromReader(strings.NewReader(markup))
   if err != nil {
@@ -15,6 +17,12 @@ func Inject(markup, scriptUrl, integrity string) (string, error) {
   doc.Find("script").Each(func(n int, s *goquery.Selection) {
     val, exists := s.Attr("src")
     if !exists {
+      return
+    }
+
+    // We don't want to override existing integrity hashes. That should require manual intervention
+    _, hasIntegrity := s.Attr("integrity")
+    if hasIntegrity {
       return
     }
 
