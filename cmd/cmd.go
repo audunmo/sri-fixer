c185f78b3ee48bbdcc8f8f85d42378818ae8c33b
Audun <audun.b.mo@protonmail.com>
2022-04-11 13:19:30 +0200

    docs: run help command as default

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,15 +1,21 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
 var RootCmd = &cobra.Command{
 	Use:   "sri-fixer",
-	Short: "SRI fixer automatically calculates and adds Subresource Integrity hashes to script tags in your HTML",
+	Short: "SRI Fixer automatically calculates and adds Subresource Integrity hashes to script tags in your HTML",
+	Long: `
+SRI Fixer automatically calculates and adds Subresource Integrity hashes to script tags in your HTML
+
+It does this by recursively searching the working directory tree for HTML files, and downloading all URLs from third-parties found in <script src=...>.
+After that, SRI Fixer will hash the downloaded javascript with sha256, sha348, and sha512.
+
+SRI Fixer updates the files in-place, adding "integrity='sha256 sha348 sha512'" and "crossorigin='anonymous'"
+	`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("SRI Fixer. Use the `run`subcommand to add SRI hashes to all script tags in the tree of this folder")
+		cmd.Help()
 	},
 }
